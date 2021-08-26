c2a832097da9ebc04dbd322c84d0bf730d4f1289
VAdithya29 <55947904+VAdithya29@users.noreply.github.com>
2021-08-26 13:49:08 +0530

    recieving data in allposts

--- src/Components/All Posts/AllPosts.js
+++ src/Components/All Posts/AllPosts.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Card from "../../layout/Card/Card";
 import "./AllPosts.css";
-// const axios = require("axios");
 
 function AllPosts() {
   useEffect(() => {
@@ -9,30 +8,23 @@ function AllPosts() {
   }, []);
 
   const [post, setPost] = useState([]);
-
-  // useEffect(() => {
-  // axios.get("http://localhost:5000").then((response) => {
-  //   setPost(response.data.users[0].name);
-  //   response.data.users.forEach((element) => {
-  //     USERS.push(element);
-  //   });
-  //   console.log(USERS);
-
-  // });
-  // });
   function fetchAllPosts() {
     fetch("http://localhost:5000/")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        // data.users.map((user) => {
-        //   return console.log(user);
         console.log(data);
         setPost(data.users);
       });
   }
-  return <Card>{post}</Card>;
+  return (
+    <Card>
+      {post.map((single) => {
+        return <Card>{single.name}</Card>;
+      })}
+    </Card>
+  );
 }
 
 export default AllPosts;
