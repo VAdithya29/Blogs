2d37e35eb55702aea52ef167166a0742d8ea6c32
VAdithya29 <55947904+VAdithya29@users.noreply.github.com>
2021-08-27 18:00:26 +0530

    Connected to nedb

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const datastore = require("nedb");
 
 const app = express();
+const database = new datastore("opinionData.db");
 app.use(cors());
+app.use(express.json());
+
+database.loadDatabase();
 
 const users1 = {
   users: [{ name: "Adithya123" }, { name: "aaa" }, { name: "bbb" }],
 };
-const users = [{ name: "Adithya123" }, { name: "aaa" }, { name: "bbb" }];
+
 app.get("/", (req, res) => {
   res.json(users1);
 });
 
-let port = 5000;
+app.get("/opinions", (req, res) => {
+  res.send("hi");
+});
 
+app.post("/opinions", (req, res) => {
+  database.insert(req.body);
+});
+
+let port = 5000;
 app.listen(port, () => {
-  console.log("Server started successfully");
+  console.log("Server started successfully on http://localhost:5000/");
 });
--- src/Components/New Post/NewPost.js
+++ src/Components/New Post/NewPost.js
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import "./NewPost.css";
 
 function BlogPost() {
+  const [title, setTitle] = useState("");
+  const [opinion, setOpinion] = useState("");
+
+  function titleChangeHandler(event) {
+    setTitle(event.target.value);
+  }
+  function opinionChangeHandler(event) {
+    setOpinion(event.target.value);
+  }
+
+  function opinionSubmitHandler(event) {
+    event.preventDefault();
+    fetch("http://localhost:5000/opinions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, opinion }),
+    });
+  }
+
   return (
     <div className="newPostForm">
-      <form>
+      <form onSubmit={opinionSubmitHandler}>
         <div className="individualElement">
-          <input type="text" placeholder="Title"></input>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={titleChangeHandler}
+          ></input>
         </div>
         <div className="individualElement">
-          <textarea placeholder="Description" rows="5"></textarea>
+          <textarea
+            placeholder="Description"
+            rows="5"
+            onChange={opinionChangeHandler}
+          ></textarea>
         </div>
         <div className="individualElement">
           <button type="submit">Submit</button>
